d55fc86d0be99c7b4bb8ee1fcb65868c940a4c5c
Andrea Stagi <stagi.andrea@gmail.com>
2014-06-08 15:14:43 +0200

    Added setResolution to speed up temperature reading

--- chickenfoot/services/temperature_sensor.py
+++ chickenfoot/services/temperature_sensor.py
@@ -9,14 +9,15 @@ class TemperatureSensor(Service):
         print 'init'
         print self.params['tp1']
         self.sensors = DallasTemperature(self.params['tp1'])
+        self.sensors.setResolution(9)
+        self.addr = self.sensors.getAddress(0)
 
     def on_shutdown(self):
         pass
 
     def on_process(self):
         self.sensors.requestTemperatures(0)
-        addr = self.sensors.getAddress(0)
-        temp = self.sensors.getTempC(addr)
+        temp = self.sensors.getTempC(self.addr)
         print temp
         self.communicator.send(str(temp))
         time.sleep(5)
